35643ec24cff3194f7a25ac988cb307a6e581dfd
Fernando Ruiz <fernando8715@hotmail.com>
2024-01-18 18:12:22 -0500

    avance en el diseño de la pagina de proyectos

---
+++ main.js
@@ -0,0 +1,13 @@
+const nav = document.querySelector('#nav');
+const cerrar = document.querySelector('#cerrar');
+const abrir = document.querySelector('#abrir');
+
+abrir.addEventListener('click', ()=>{
+    nav.classList.add('visible');
+    abrir.classList.add('hidden');
+});
+
+cerrar.addEventListener('click', ()=>{
+    nav.classList.remove('visible')
+    abrir.classList.remove('hidden');
+})
